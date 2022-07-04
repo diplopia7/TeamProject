729084da2a7650a4090ed0584b81987f35ea8d60
jonghyun5646 <87743536+jonghyun5646@users.noreply.github.com>
2022-07-04 09:40:03 +0900

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.pay, name='pay'),
-    path('recharge/', views.recharge, name='recharge'),
-    path('rechargeok/', views.rechargeok, name='rechargeok'),
-]
