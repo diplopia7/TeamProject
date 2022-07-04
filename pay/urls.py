7e198ef3f24466fa98cb2ab24ca0a51a60a01c91
LeeByeongHwi <diplopia7@gmail.com>
2022-07-04 15:23:19 +0900

    이병휘-character 수정

--- pay/urls.py
+++ pay/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pay, name='pay'),
+    path('pay/', views.pay, name='pay'),
     path('recharge/', views.recharge, name='recharge'),
     path('rechargeok/', views.rechargeok, name='rechargeok'),
 ]
